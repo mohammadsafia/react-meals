efcb05041581736c3bb5faaf0054a8e2b7c35ed1
Mohammad Safia <mohammad.safia@meccasoft.com>
2022-08-12 22:21:40 +0300

    create header component

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import mealsImage from 'assets/meals.jpg'
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+interface HeaderProps {
+}
+
+const Header: React.FC<HeaderProps> = () => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt=""/>
+            </div>
+        </>
+    );
+};
+
+export default Header;
